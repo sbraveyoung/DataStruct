5a2f3fe8ab6c66e75248e2a7f09ccdbac43e558d
SmartBrave <779850695@qq.com>
2016-06-25 01:08:15 +0800

    2016-06-25-01-08-15

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -167,7 +167,185 @@ public:
         cout<<"Depth: ";
         return _Depth(root);
     }
+    void PrintEdge1()
+    {
+        if(root==NULL)
+            return ;
+        vector<vector<Node<T>*> > edge;
+        edge.resize(_Depth(root));
+        _setEdge(root,0,edge);
+        int i;
+        for(i=0;i<edge.size();++i)
+        {
+            cout<<edge[i][0]->data<<" ";
+        }
+        _printLeafNotInEdge(root,0,edge);
+        for(i=edge.size()-1;i>=0;--i)
+        {
+            if(edge[i][0]!=edge[i][1])
+                cout<<edge[i][1]->data<<" ";
+        }
+        cout<<endl;
+    }
+    void PrintEdge2()
+    {
+        _PrintEdge2(root);
+    }
+    int size()
+    {
+        return _size(root);
+    }
+    void PrintTree()
+    {
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //error
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        int widthspace=3;
+        int widthword=5;
+        if(root==NULL)
+            return;
+        queue<Node<T>*> q1;
+        queue<Node<T>*> q2;
+        Node<T>* cur=root;
+        q1.push(cur);
+        int level=0;
+        bool flag=true;//栈中是否还有不为空的节点
+        while(flag && (!q1.empty() || !q2.empty()))
+        {
+            flag==false;
+            if(!q1.empty())
+            {
+                level++;
+                printf("%*s",(_Depth(root)-level)*widthspace," ");
+                while(!q1.empty())
+                {
+                    cur=q1.front();
+                    q1.pop();
+                    //cout<<cur->data<<" ";
+                    if(cur!=NULL)
+                        printf("%*d",widthword,cur->data);
+                    else
+                        printf("%*s",widthword," ");
+                    printf("%*s",widthspace," ");
+                    if(cur!=NULL)
+                    {
+                        if(cur->left!=NULL)
+                        {
+                            flag=true;
+                            q2.push(cur->left);
+                        }
+                        if(cur->right!=NULL)
+                        {
+                            flag=true;
+                            q2.push(cur->right);
+                        }
+                    }
+                    else
+                    {
+                        q2.push(NULL);
+                        q2.push(NULL);
+                    }
+                }
+                cout<<endl;
+            }
+            if(!q2.empty())
+            {
+                level++;
+                printf("%*s",(_Depth(root)-level)*widthspace," ");
+                while(!q2.empty())
+                {
+                    cur=q2.front();
+                    q2.pop();
+                    //cout<<cur->data<<" ";
+                    if(cur!=NULL)
+                        printf("%*d",widthword,cur->data);
+                    else
+                        printf("%*s",widthword," ");
+                    printf("%*s",widthspace," ");
+                    if(cur!=NULL)
+                    {
+                        if(cur->left!=NULL)
+                        {
+                            flag=true;
+                            q1.push(cur->left);
+                        }
+                        if(cur->right!=NULL)
+                        {
+                            flag=true;
+                            q1.push(cur->right);
+                        }
+                    }
+                    else
+                    {
+                        q1.push(NULL);
+                        q1.push(NULL);
+                    }
+                }
+                cout<<endl;
+            }
+        }
+        cout<<endl;
+    }
 private:
+    int _size(Node<T>* root)
+    {
+        if(root==NULL)
+            return 0;
+        return _size(root->left) + _size(root->right) +1;
+    }
+    void _PrintEdge2(Node<T>* root)
+    {
+        if(root==NULL)
+            return;
+        cout<<root->data<<" ";
+        if(root->left!=NULL && root->right!=NULL)
+        {
+            _printLeftEdge(root->left,true);
+            _printRightEdge(root->right,true);
+        }
+        else
+        {
+            _PrintEdge2(root->left!=NULL?root->left:root->right);
+        }
+        cout<<endl;
+    }
+    void _printLeftEdge(Node<T>* root,bool print)
+    {
+        if(root==NULL)
+            return;
+        if(print || (root->left==NULL && root->right==NULL))
+            cout<<root->data<<" ";
+        _printLeftEdge(root->left,true);
+        _printLeftEdge(root->right,print && root->left==NULL?true:false);
+    }
+    void _printRightEdge(Node<T>* root,bool print)
+    {
+        if(root==NULL)
+            return;
+        _printRightEdge(root->left,print && root->right==NULL?true:false);
+        _printRightEdge(root->right,true);
+        if(print || (root->left==NULL && root->right==NULL))
+            cout<<root->data<<" ";
+    }
+    void _printLeafNotInEdge(Node<T>* root,int pos,vector<vector<Node<T>*> > edge)
+    {
+        if(root==NULL)
+            return;
+        if(root->left==NULL && root->right==NULL && root!=edge[pos][0] && root!=edge[pos][1])
+            cout<<root->data<<" ";
+        _printLeafNotInEdge(root->left,pos+1,edge);
+        _printLeafNotInEdge(root->right,pos+1,edge);
+    }
+    void _setEdge(Node<T>* root,int pos,vector<vector<Node<T>*> >& edge)
+    {
+        if(root==NULL)
+            return;
+        edge[pos].resize(2);
+        edge[pos][0]=edge[pos][0]==NULL?root:edge[pos][0];
+        edge[pos][1]=root;
+        _setEdge(root->left,pos+1,edge);
+        _setEdge(root->right,pos+1,edge);
+    }
     int _Depth(const Node<T>* root)
     {
         if(root==NULL)
@@ -238,7 +416,7 @@ public:
     Node<T>* root;
 };
 
-void test()
+void test1()
 {
     int pre[]={1,2,4,8,9,5,10,3,6,7};
     int in[]={8,4,9,2,10,5,1,6,3,7};
@@ -254,10 +432,23 @@ void test()
     btree.PostOrder_NR();
     btree.LevelOrder();
     cout<<btree.Depth()<<endl;
+    btree.PrintEdge1();
+}
+
+void test2()
+{
+    int pre[]={1,2,4,7,8,11,13,14,3,5,9,12,15,16,10,6};
+    int in[]={2,7,4,8,13,11,14,1,15,12,16,9,5,10,3,6};
+    BinaryTree<int> btree;
+    btree.CreateWithPre(pre,in,sizeof(pre)/sizeof(pre[0]));
+    //btree.PrintEdge1();
+    //btree.PrintEdge2();
+    btree.PrintTree();
 }
 
 int main()
 {
-    test();
+    //test1();
+    test2();
     return 0;
 }
